ba2c01dda5647b62b4e10db070328e178539a23f
manuligit <riina.vaisanen@gmail.com>
2017-10-17 21:29:07 +0300

    Update project for python3

---
+++ README.md
@@ -0,0 +1,1 @@
+# The usage of buildings in Helsinki
--- test.py
+++ test.py
@@ -1,15 +1,17 @@
+#!/usr/bin/env python3
 # coding=utf-8
-import pandas
-import px_reader
+import pandas as pd
 import geopandas as gpd
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
+# This project requires python3: 
+if sys.version_info[0] < 3:
+    raise Exception("Python 3 or a more recent version is required.")
 
 # Read data
-px_obj = px_reader.Px('A01S_HKI_Askan_rvuosi.px')
-df = px_obj.pd_dataframe()
-#df.to_csv('items.csv', encoding="utf-8")
+data = pd.read_csv('./kokotun.csv')
 
 # Read map data
 # Use Geopandas to read the MapInfo files:
@@ -18,10 +20,5 @@ df = px_obj.pd_dataframe()
 # a_hk_pie.MAP  (Stores the graphic and geographic information needed to display each vector feature on a map)
 # a_hk_pie.tab (The ASCII file that is the link between all other files and holds information about the type of data set file )
 kartta = gpd.read_file('./Seutukartta/SK-2016-avoin/a_hk_pie.tab')
-#kartta.head()
-
 kartta.plot()
 plt.show()
-#print('A_HK_per')
-# Mikäli käytetään suoraan PcAxis –tiedostoa, aluenimi vaihdetaan aluekoodiksi 
-# ”Muokkaa>Vaihda koodi/teksti” valikosta, jossa vaihdetaan Alue-muuttujan esitystavaksi koodi. 
