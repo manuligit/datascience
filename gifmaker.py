38871f69421b63b5c52d017285725afb378acf9d
Krista Väänänen <krista.vaananen@helsinki.fi>
2017-10-25 19:24:10 +0300

    gif created from maps

---
+++ gifmaker.py
@@ -0,0 +1,17 @@
+import imageio
+
+images = []
+images.append(imageio.imread('maps/until1920_text.png'))
+images.append(imageio.imread('maps/1921-1929_text.png'))
+images.append(imageio.imread('maps/1930-1939_text.png'))
+images.append(imageio.imread('maps/1940-1949_text.png'))
+images.append(imageio.imread('maps/1950-1959_text.png'))
+images.append(imageio.imread('maps/1960-1969_text.png'))
+images.append(imageio.imread('maps/1970-1979_text.png'))
+images.append(imageio.imread('maps/1980-1989_text.png'))
+images.append(imageio.imread('maps/1990-1999_text.png'))
+images.append(imageio.imread('maps/2000-2009_text.png'))
+images.append(imageio.imread('maps/2010-2016_text.png'))
+images.append(imageio.imread('maps/total_text.png'))
+
+imageio.mimsave('movie.gif', images,  duration=2)
